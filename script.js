ada725aa5aa1d8d73bd54a4632277321e586efce
Jeremy Bertin <jeremy.bertin76@hotmail.fr>
2020-12-02 21:59:15 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+$(function(){
+  var $mainMenuItems = $('#main-menu ul').children('li'),
+  totalMainMenuItems = $mainMenuItems.length,
+  openedIndex = 2,
+
+  init = function(){
+    bindEvents();
+    if(validIndex(openedIndex))
+      animateItem($mainMenuItems.eq(openedIndex), true, 700);
+  },
+
+  bindEvents = function(indexToCheck){
+    $mainMenuItems.children('.images').click(function(){
+      var newIndex = $(this).parent().index();
+      checkAndAnimateItem(newIndex);
+    });
+
+    $(".button").hover(
+      function(){
+        $(this).addClass("hovered");
+    },
+      function(){
+        $(this).removeClass("hovered");
+    }
+  );
+
+  $(".button").click(function(){
+    var newIndex = $(this).index();
+    checkAndAnimateItem(newIndex);
+  });
+
+  },
+
+    validIndex = function(indexToCheck){
+      return (indexToCheck >= 0) && (indexToCheck < totalMainMenuItems);
+    },
+
+    animateItem = function($item, toOpen, speed){
+      var $colorImage = $item.find('.color'),
+      itemParam = toOpen ? {width:"420px"}:{width:"140px"},
+      colorImageParam = toOpen ? {left: '0px'}:{left: '140px'};
+      $colorImage.animate(colorImageParam, speed);
+      $item.animate(itemParam, speed);
+},
+
+checkAndAnimateItem = function(indexToChaeckAndAnimate){
+  if(openedIndex === indexToChaeckAndAnimate){
+    animateItem($mainMenuItems.eq(indexToChaeckAndAnimate), false, 250);
+    openedIndex = -1;
+  } else {
+    if(validIndex(indexToChaeckAndAnimate)){
+      animateItem($mainMenuItems.eq(openedIndex), false, 250);
+      openedIndex = indexToChaeckAndAnimate;
+      animateItem($mainMenuItems.eq(openedIndex), true, 250);
+    }
+  }
+};
+
+  init();
+});
